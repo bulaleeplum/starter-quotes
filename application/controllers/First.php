f4f98e7b88fbb21c3cc1ff8972615b6e36c6495c
Scott-Plummer <Scott.Plummer18@gmail.com>
2016-02-02 17:13:36 -0800

    Fixed sleep link

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -29,6 +29,18 @@ class First extends Application {
 
         $this->render();
     }
+
+    function zzz() {
+        $this->data['pagebody'] = 'justone';   // this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->all();
+        $authors = array();
+        $record = $this->quotes->get(1);
+        $this->data = array_merge($this->data, $record);
+        $this->data['authors'] = $authors;
+
+        $this->render();
+    }
 }
 
 /* End of file Welcome.php */
